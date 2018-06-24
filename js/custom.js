70120f4d67ea8c9b20be5c37407cd6908d43bbde
evgenkarp01 <ewal@list.ru>
2018-06-24 13:30:43 -0700

    24062018

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,37 @@ $( document ).ready(function() {
         speed: 700,
     });
     
+   
+    
+    
+    
+    var $menu = $("header");
+
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+                $menu.removeClass("default").addClass("fixed");
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.removeClass("fixed").addClass("default");
+            }
+        });//scroll
+    
+    
+    
+    
+$('.furniture').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.furniture-pagination'
+  });
+  $('.furniture-pagination').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.furniture',
+    centerMode: true,
+    focusOnSelect: true
+  });
     
     
     
@@ -30,6 +61,32 @@ $( document ).ready(function() {
     
     
     
+    
+    
+    
+    
+    $('.sldr-video-review').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.review-preview'
+    });
+    $('.review-preview').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.sldr-video-review, .auth-sldr',
+        centerMode: true,
+        focusOnSelect: true
+    });
+    
+    $('.auth-sldr').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.review-preview'
+    });
     
     
     

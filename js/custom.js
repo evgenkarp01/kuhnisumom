dd0643f65f94d255652c2d83017f5138f902828d
evgenkarp01 <ewal@list.ru>
2018-06-25 13:59:02 +0000

    25062018

--- js/custom.js
+++ js/custom.js
@@ -91,11 +91,51 @@ $('.furniture').slick({
     
     
     
+    var locations = [
+            ["Ульяновская д.25", 47.216003, 39.714761, "http://ewal-web.ru/assets/site/img/map-marker-kuhnisumom.png"],
+        ];
+        var map = new google.maps.Map(document.getElementById('mapa'), {
+            zoom: 18,
+            styles: 
+            [
+                {"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},
+                {"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},
+                {"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+                {"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},
+                {"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}
+            ],
+            center: new google.maps.LatLng(47.216003, 39.714761),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map,
+                icon: {
+                    url: locations[i][3],
+                    scaledSize: new google.maps.Size(50, 75)
+                }
+            });
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
     
     
     
-    
-    
+    $("#navToggle").click(function() {
+	    $(this).toggleClass("active");
+	    $(".overlay").toggleClass("open");
+	    // this line ▼ prevents content scroll-behind
+	    $("body").toggleClass("locked"); 
+	});
     
     
     
